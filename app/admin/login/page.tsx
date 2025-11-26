56b65ce7b310304485d187bc928ecca665062c0a
Elijah Amodu <elijahamodu49@gmail.com>
2025-11-26 21:37:19 +0100

    updasted login page

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -2,28 +2,11 @@
 
 import { useState } from "react";
 import LoginPage from "@/component/login-page";
-import SignupFlow from "@/component/signup-flow";
 
 export default function page() {
   const [isSignup, setIsSignup] = useState(false);
 
   return (
-    // <>
-    //   {isSignup ? (
-    //     <div className="min-h-screen bg-[#F0F3F9] flex items-center justify-center p-4">
-    //       <SignupFlow onBackToLogin={() => setIsSignup(false)} />
-    //     </div>
-    //   ) : (
-    //     <div
-    //       className="min-h-screen flex items-center justify-center p-4"
-    //       style={{
-    //         backgroundImage: "url('/login-background.svg')",
-    //       }}
-    //     >
-    //       <LoginPage onSignupClick={() => setIsSignup(true)} />
-    //     </div>
-    //   )}
-    // </>
     <div
       className="min-h-screen flex items-center justify-center p-4"
       style={{
